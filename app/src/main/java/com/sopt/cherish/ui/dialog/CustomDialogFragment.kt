3cfe60a9865105e24ebb279aed73a48c8ca7d9b1
SSong-develop <thdgnsrl123@naver.com>
2021-01-02 18:49:26 +0900

    [chore]: 나영이 버전 테스트

--- app/src/main/java/com/sopt/cherish/ui/dialog/CustomDialogFragment.kt
+++ app/src/main/java/com/sopt/cherish/ui/dialog/CustomDialogFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.DialogFragment
 import com.sopt.cherish.R
@@ -26,7 +25,6 @@ class CustomDialogFragment(
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(layoutResId, container, false)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
 
         return when (layoutResId) {
             R.layout.sample_lottie -> {
