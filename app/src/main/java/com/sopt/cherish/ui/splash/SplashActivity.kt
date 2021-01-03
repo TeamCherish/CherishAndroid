47aa127c004a7bdb646ab01d1ddf5190206d254c
SSong-develop <thdgnsrl123@naver.com>
2021-01-03 18:15:28 +0900

    [chore]: permission에 대한 내 생각

--- app/src/main/java/com/sopt/cherish/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/sopt/cherish/ui/splash/SplashActivity.kt
@@ -13,6 +13,9 @@ import com.sopt.cherish.util.extension.shortToast
  * 스플래쉬 시점이 아닌 Tutorial이 끝난 시점에 하는게 맞는거 같다.
  * 권한 허가를 어느 뷰에서 해줄 지는 다시 생각해보자
  * test방법 : Splash Activity를 처음 시작하는 activity로 둔 후 실행
+ *
+ * PermissionUtil 이 필요없을 수 도 있겠지만 , 생각해야할 것은 유저가 permission을 허가하지 않는 경우
+ * 가 존재한다는 것을 인지하고 개발해야하는 것이 중요하다는게 생각이다.
  */
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
