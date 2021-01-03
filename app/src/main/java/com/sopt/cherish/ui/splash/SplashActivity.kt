46ec56e9a2ec8626d5c19a7c9798bb8987330d10
SSong-develop <thdgnsrl123@naver.com>
2021-01-03 18:11:01 +0900

    [feat]: PermissionUtil author MJStudio , Thanks for your kindness

--- app/src/main/java/com/sopt/cherish/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/sopt/cherish/ui/splash/SplashActivity.kt
@@ -1,12 +1,51 @@
 package com.sopt.cherish.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.cherish.R
+import com.sopt.cherish.ui.tutorial.TutorialActivity
+import com.sopt.cherish.util.PermissionUtil
+import com.sopt.cherish.util.extension.shortToast
 
+/**
+ * Created on 2020-01-03 by SSong-develop
+ * 스플래쉬 시점이 아닌 Tutorial이 끝난 시점에 하는게 맞는거 같다.
+ * 권한 허가를 어느 뷰에서 해줄 지는 다시 생각해보자
+ * test방법 : Splash Activity를 처음 시작하는 activity로 둔 후 실행
+ */
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        requestCherishPermissions()
+    }
+
+    private fun requestCherishPermissions() {
+        PermissionUtil.requestCherishPermission(this, object : PermissionUtil.PermissionListener {
+            override fun onPermissionGranted() {
+                startTutorialActivityAndFinish()
+            }
+
+            override fun onPermissionShouldBeGranted(deniedPermissions: List<String>) {
+                shortToast(this@SplashActivity, "권한 허용이 안되어있습니다. $deniedPermissions")
+                openSettings()
+            }
+
+            override fun onAnyPermissionPermanentlyDenied(deniedPermissions: List<String>, permanentDeniedPermissions: List<String>) {
+                shortToast(this@SplashActivity, "권한 허용이 영구적으로 거부되었습니다. $permanentDeniedPermissions")
+                openSettings()
+            }
+        })
+    }
+
+    private fun openSettings() {
+        PermissionUtil.openPermissionSettings(this)
+    }
+
+    private fun startTutorialActivityAndFinish() {
+        startActivity(Intent(this, TutorialActivity::class.java))
+        finish()
     }
 }
