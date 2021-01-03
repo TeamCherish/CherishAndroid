b5d006b0775588b3523b98f3ee602ac173a7a4e2
YEJIN-LILY <gristar98@naver.com>
2021-01-03 19:41:34 +0900

    [feat]: HomeFragment 뷰페이저 삭제

--- app/src/main/java/com/sopt/cherish/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/cherish/ui/main/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.cherish.R
 import com.sopt.cherish.databinding.ActivityMainBinding
-import com.sopt.cherish.ui.adapter.MainBottomNaviAdapter
 import com.sopt.cherish.ui.main.home.HomeFragment
 import com.sopt.cherish.ui.main.manageplant.ManagePlantFragment
 import com.sopt.cherish.ui.main.setting.SettingFragment
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewpagerAdapter: MainBottomNaviAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,24 +19,33 @@ class MainActivity : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
 
-        viewpagerAdapter=MainBottomNaviAdapter(supportFragmentManager)
-        viewpagerAdapter.fragments=listOf(
-                HomeFragment(),
-                ManagePlantFragment(),
-                SettingFragment()
-        )
+        val homeFragment=HomeFragment()
+        val managePlantFragment=ManagePlantFragment()
+        val settingFragment=SettingFragment()
 
-        binding.bottomViewpager.adapter=viewpagerAdapter
+        supportFragmentManager.beginTransaction()
+                .add(R.id.main_fragment_container,homeFragment).commit()
+
 
         binding.mainBottomNavi.setOnNavigationItemSelectedListener{
-            var index:Int by Delegates.notNull<Int>()
+            val transAction=supportFragmentManager.beginTransaction()
+
             when(it.itemId){
-                R.id.main_home->index=0
-                R.id.main_person->index=1
-                R.id.main_more->index=2
+                R.id.main_home-> {
+                    transAction.replace(R.id.main_fragment_container, homeFragment).commit()
+                    true
+                }
+                R.id.main_manage_plant->{
+                    transAction.replace(R.id.main_fragment_container, managePlantFragment).commit()
+                    true
+                }
+                R.id.main_setting->{
+                    transAction.replace(R.id.main_fragment_container, settingFragment)
+                    true
+                }else->{
+                    throw AssertionError()
+                }
             }
-            binding.bottomViewpager.currentItem=index
-            true
         }
     }
 }
