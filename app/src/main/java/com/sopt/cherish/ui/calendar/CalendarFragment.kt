ca154d538fc3fad6d33da6e928dbee30bcb2faf8
SSong-develop <thdgnsrl123@naver.com>
2021-01-07 01:07:16 +0900

    [feat]: calendarFragment 뼈대 작업 해놓음 & DetailPlant 뷰모델 작업시작

--- app/src/main/java/com/sopt/cherish/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/sopt/cherish/ui/calendar/CalendarFragment.kt
@@ -33,14 +33,13 @@ class CalendarFragment : Fragment() {
             DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
 
         initializeCalendar(binding)
-        addCalendarListener(binding)
-
         return binding.root
     }
 
     private fun initializeCalendar(binding: FragmentCalendarBinding) {
-        // 서버에서 데이터 가져온 거 check
+        // need 서버에서 데이터 가져온 거 check
 
+        // Color value 또한
         val colorPinkSub = ContextCompat.getColor(requireContext(), R.color.cherish_green_sub)
         val colorGreenSub = ContextCompat.getColor(requireContext(), R.color.cherish_pink_sub)
 
@@ -64,16 +63,30 @@ class CalendarFragment : Fragment() {
         dummyWateringListDay.forEach {
             binding.calendarView.addDecorator(DotDecorator(colorPinkSub, it))
         }
-    }
 
-    private fun addCalendarListener(binding: FragmentCalendarBinding) {
+        // date ClickListener
         binding.calendarView.setOnDateChangedListener { widget, date, selected ->
-            // widget : materialcalendar에 대한 widget이 그냥 넘어오는 것 같다. 자세히는 x
+            // widget : materialcalendar에 대한 widget이 그냥 넘어오는 것 같다. 자세히는 모름
             // date : 선택된 날짜
             // selected : boolean 값으로 select가 되었는지 안되었는지를 판단하게 해주는 값
 
-            binding.calendarViewSelectedDate.text = "${date.year}년 ${date.month}월 ${date.day}일"
+            // 선택되면 그에 따라서 메모가 나온다던지 ~ 그런기능을 해주면 될 거 같고
+            showDate(binding, date)
+            showMemo(binding)
+            showChips(binding)
         }
+    }
+
+    private fun showDate(binding: FragmentCalendarBinding, date: CalendarDay) {
+        binding.calendarViewSelectedDate.text = "${date.year}년 ${date.month}월 ${date.day}일"
+    }
+
+    private fun showChips(binding: FragmentCalendarBinding) {
+        // dummy Data로 구현하면 될 듯
+    }
+
+    private fun showMemo(binding: FragmentCalendarBinding) {
+
     }
 
 }
---
+++ app/src/main/java/com/sopt/cherish/ui/datail/DetailPlantViewModel.kt
@@ -0,0 +1,5 @@
+package com.sopt.cherish.ui.datail
+
+import androidx.lifecycle.ViewModel
+
+class DetailPlantViewModel : ViewModel()
