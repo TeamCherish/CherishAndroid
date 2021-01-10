10e14bd764caec060d44b684b6c5c3ea483c4e83
ny2060 <dpswpfskdud@naver.com>
2021-01-11 00:44:32 +0900

    [feat]:signin_view 완료

--- app/src/main/java/com/sopt/cherish/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/sopt/cherish/ui/signin/SignInActivity.kt
@@ -1,12 +1,27 @@
 package com.sopt.cherish.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.cherish.R
+import com.sopt.cherish.databinding.ActivitySignInBinding
+import com.sopt.cherish.ui.main.MainActivity
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
