ecd11332df4189b3b53ae3165de343e53e93852c
SSong-develop <thdgnsrl123@naver.com>
2021-02-21 16:18:57 +0900

    [feat] : detail plant watering flow 작업!!!

--- app/src/main/java/com/sopt/cherish/ui/detail/DetailPlantFragment.kt
+++ app/src/main/java/com/sopt/cherish/ui/detail/DetailPlantFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
