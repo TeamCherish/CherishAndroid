e218d758b0c139762cc286bb700de58f1ab17413
SSong-develop <thdgnsrl123@naver.com>
2021-03-15 22:10:49 +0900

    [feat] : 필요없는 클래스 정리

--- app/src/main/java/com/sopt/cherish/repository/DetailPlantRepository.kt
+++ app/src/main/java/com/sopt/cherish/repository/DetailPlantRepository.kt
@@ -9,13 +9,9 @@ class DetailPlantRepository(
     private val calendarAPI: CalendarAPI,
     private val reviewAPI: ReviewAPI
 ) {
-    // repository가 알고있어야 할 것은 retrofitService interface
     suspend fun fetchCalendarData(cherishId: Int): CalendarRes {
         return calendarAPI.getCalendarData(cherishId)
     }
-
-    suspend fun sendReviewData(reviewWateringReq: ReviewWateringReq) =
-        reviewAPI.reviewWatering(reviewWateringReq)
 
     suspend fun sendReviseReviewData(reviseReviewReq: ReviseReviewReq) =
         reviewAPI.reviseReview(reviseReviewReq)
--- app/src/main/java/com/sopt/cherish/repository/EnrollmentRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.sopt.cherish.repository
-
-/**
- * Create on 01-11 by SSong-develop
- */
-class EnrollmentRepository
