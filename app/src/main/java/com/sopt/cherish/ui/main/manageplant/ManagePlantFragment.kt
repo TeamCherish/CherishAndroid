610a28976401fd7ad5626f52270db552830a3af9
YEJIN-LILY <gristar98@naver.com>
2021-03-20 14:14:58 +0900

    [chore]: conflict solved

--- app/src/main/java/com/sopt/cherish/ui/main/manageplant/ManagePlantFragment.kt
+++ app/src/main/java/com/sopt/cherish/ui/main/manageplant/ManagePlantFragment.kt
@@ -12,7 +12,6 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.marginTop
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
