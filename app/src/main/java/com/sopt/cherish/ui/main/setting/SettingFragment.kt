3b99878ec3b91372c4d26003d831fed2b45bad97
안나영 <dpswpfskdud@naver.com>
2021-04-04 20:41:24 +0900

    aboutcherish구현

--- app/src/main/java/com/sopt/cherish/ui/main/setting/SettingFragment.kt
+++ app/src/main/java/com/sopt/cherish/ui/main/setting/SettingFragment.kt
@@ -66,6 +66,11 @@ class SettingFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.constraintLayoutAboutCherish.setOnClickListener {
+            val intent = Intent(context,AboutCherishActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.constraintLayoutInfo.setOnClickListener {
             val intent = Intent(
                 Intent.ACTION_VIEW,
