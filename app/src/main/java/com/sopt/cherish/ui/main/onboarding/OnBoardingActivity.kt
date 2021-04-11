99e590dfbec64086b8fe8a8fe32183cfe8ff2b32
YEJIN-LILY <gristar98@naver.com>
2021-04-11 23:26:15 +0900

    [fix] : 온보딩 분기처리

--- app/src/main/java/com/sopt/cherish/ui/main/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/sopt/cherish/ui/main/onboarding/OnBoardingActivity.kt
@@ -1,21 +1,13 @@
 package com.sopt.cherish.ui.main.onboarding
 
-import android.graphics.Color
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.sopt.cherish.R
-import com.sopt.cherish.databinding.ActivityAboutcherishBinding
 import com.sopt.cherish.databinding.ActivityOnboardingBinding
-import com.sopt.cherish.databinding.FragmentOnboardingBinding
-import com.sopt.cherish.ui.main.onboarding.adapter.OnBoardingViewPagerAdapter
-import com.zhpan.indicator.enums.IndicatorSlideMode
-import com.zhpan.indicator.enums.IndicatorStyle
 
 class OnBoardingActivity: AppCompatActivity() {
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +25,4 @@ class OnBoardingActivity: AppCompatActivity() {
         transaction.replace(R.id.onBoardingFragment, fragment)
         transaction.commit()
     }
-
-
-
-
-
 }
